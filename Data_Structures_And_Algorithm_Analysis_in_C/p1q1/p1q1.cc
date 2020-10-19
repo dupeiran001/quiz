7c0fee4edb8f8887f08fef98cc09a59131f39754
dpr <2083856903@qq.com>
2020-10-19 18:47:12 +0800

    init p1q1

--- Data_Structures_And_Algorithm_Analysis_in_C/p1q1/p1q1.cc
+++ Data_Structures_And_Algorithm_Analysis_in_C/p1q1/p1q1.cc
@@ -1,4 +1,9 @@
-
+// 编写一个程序解决选择问题。令K=N/2 。画出表格显示你的程序对于N为不同值的运行时间
+//
+// author  : 杜沛然
+// licence : GPL V3
+// e-mail  : 2083856903@qq.com
+// date    : 20201019
 
 int main(int argc , char** argv)
 {
